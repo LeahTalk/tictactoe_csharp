7aec2a005b4ef09a884e8de7ef92a7b3b3708c30
= <lctalkov@yahoo.com>
2019-10-22 00:28:54 -0700

    Very simple console tictactoe game in csharp

---
+++ Board.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TicTacToe
+{
+    class Board
+    {
+        private string[,] board;
+        private bool gameEnd;
+        public Board() {
+            board = new string[3,3];
+            gameEnd = false;
+        }
+
+        public bool gameOver {
+            get {
+                return gameEnd;
+            }
+        }
+
+        public bool checkWin(string curPlayer) {
+            if(checkRows(curPlayer) || checkCols(curPlayer) || checkDiagonals(curPlayer)) {
+                gameEnd = true;
+                return true;
+            }
+            return false;
+        }
+
+        public void displayBoard() {
+            for(int row = 0; row < 3; row++) {
+                Console.Write($"{board[row, 0]} ");
+                for(int col = 1; col < 3; col++) {
+                    Console.Write(" | ");
+                    Console.Write(board[row, col]);
+                }
+                Console.WriteLine();
+                if(row != 2) {
+                    Console.WriteLine("---------");
+                }
+            }
+        }
+
+        public bool placePiece(string curPlayer, int rowNum, int colNum) {
+            if(board[rowNum,colNum] == null) {
+                board[rowNum, colNum] = curPlayer;
+                return true;
+            }
+            return false;
+        }
+
+        public bool checkRows(string curPlayer) {
+            for(int row = 0; row < 3; row++) {
+                bool fullRow = true;
+                for(int col = 0; col < 3; col++) {
+                    if(board[row, col] != curPlayer) {
+                        fullRow = false;
+                    }
+                }
+                if(fullRow) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool checkCols(string curPlayer) {
+            for(int col = 0; col < 3; col++) {
+                bool fullCol = true;
+                for(int row = 0; row < 3; row++) {
+                    if(board[row, col] != curPlayer) {
+                        fullCol = false;
+                    }
+                }
+                if(fullCol) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool checkDiagonals(string curPlayer) {
+            bool diagOne = true;
+            bool diagTwo = true;
+            for(int i = 0; i < 3; i++) {
+                if(board[i, i] != curPlayer) {
+                    diagOne = false;
+                }
+                if(board[i, 2 - i] != curPlayer) {
+                    diagTwo = false;
+                }
+            }
+            return diagOne || diagTwo;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] characters = new string[] {"X", "O"};
+            Board game = new Board();
+            int playerIndex = 0;
+            string curPlayer = characters[playerIndex];
+            while(!game.gameOver) {
+                Console.WriteLine($"It is player {characters[playerIndex]}'s turn!");
+                bool validMove = false;
+                while(!validMove) {
+                    bool validRow = false;
+                    bool validCol = false;
+                    int rowNum = 0;
+                    int colNum = 0;
+                    while(!validRow) {
+                        game.displayBoard();
+                        Console.WriteLine("Please choose a row number: ");
+                        rowNum = Int32.Parse(Console.ReadLine());
+                        if(rowNum < 1 || rowNum > 3) {
+                            Console.WriteLine("Please select a valid row number!");
+                        }
+                        else {
+                            validRow = true;
+                        }
+                    }
+                    while(!validCol) {
+                        Console.WriteLine("Please choose a column number: ");
+                        colNum = Int32.Parse(Console.ReadLine());
+                        if(colNum < 1 || colNum > 3) {
+                            Console.WriteLine("Please select a valid column number!");
+                        }
+                        else {
+                            validCol = true;
+                        }
+                    }
+                    if(game.placePiece(curPlayer, rowNum - 1, colNum - 1)) {
+                        validMove = true;
+                    }
+                    else {
+                        Console.WriteLine("That square is already taken! Please choose another space.");
+                    }
+                }
+                if(!game.checkWin(curPlayer)) {
+                    playerIndex = (playerIndex + 1) % 2;
+                    curPlayer = characters[playerIndex];
+                }
+            }
+            game.displayBoard();
+            Console.WriteLine($"Player {curPlayer} won!!!");
+        }
+    }
+}
